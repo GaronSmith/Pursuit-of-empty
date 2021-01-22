9db633ba57d9a85fc3dc5308880360649ed5c216
GaronSmith <wgaronsmith13@gmail.com>
2021-01-22 13:08:32 -0500

    completed backend auth

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,9 +5,5 @@ const router = express.Router();
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
 
 module.exports = router;
