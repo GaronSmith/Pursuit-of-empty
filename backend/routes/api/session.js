f6a50031c8aa64fd9d2d5d035cb9db68b99797c6
GaronSmith <wgaronsmith13@gmail.com>
2021-01-22 13:01:19 -0500

    validation errors for login

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,11 +3,23 @@ const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors,
+];
 
-router.post('/', asyncHandler(async (req, res, next) => {
+router.post('/', validateLogin, asyncHandler(async (req, res, next) => {
         const { credential, password } = req.body;
 
         const user = await User.login({ credential, password });
@@ -46,5 +58,7 @@ router.get(
         } else return res.json({});
     }
 );
+
+
 
 module.exports = router;
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,7 +1,5 @@
 const { validationResult } = require('express-validator');
 
-// middleware for formatting errors from express-validator middleware
-// (to customize, see express-validator's documentation)
 const handleValidationErrors = (req, _res, next) => {
     const validationErrors = validationResult(req);
 
