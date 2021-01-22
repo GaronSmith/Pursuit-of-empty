2d96522b52559a0533a705377e0eea4c0d1b57f0
GaronSmith <wgaronsmith13@gmail.com>
2021-01-22 12:38:17 -0500

    added get session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -34,4 +34,17 @@ router.delete('/', (_req, res) => {
     }
 );
 
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
+    }
+);
+
 module.exports = router;
