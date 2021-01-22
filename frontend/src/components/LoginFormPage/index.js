64c620a6304f57e482f5ea26e52464254e8640bc
GaronSmith <wgaronsmith13@gmail.com>
2021-01-22 17:20:51 -0500

    added slight css to login form

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Redirect, useHistory } from "react-router-dom"
 import { login } from "../../store/session"
+import './LoginForm.css'
 
 
 const LoginFormPage = () =>{
