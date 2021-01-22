d0e5bcfaab234f10e49cccac6a56d7196f346f00
GaronSmith <wgaronsmith13@gmail.com>
2021-01-22 15:46:18 -0500

    added index.html server

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,29 @@ const router = express.Router();
 
 router.use('/api', apiRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    router.get('/', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+    router.use(express.static(path.resolve("../frontend/build")));
+
+    router.get(/^(?!\/?api).*/, (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    router.get('/api/csrf/restore', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.json({});
+    });
+}
 
 module.exports = router;
