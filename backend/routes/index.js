f3fe8d83782434a0eb81c8d907a81323bb3d329a
GaronSmith <wgaronsmith13@gmail.com>
2021-01-25 12:21:51 -0500

    added routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,32 +1,34 @@
 const express = require('express');
-const apiRouter = require('./api')
-
 const router = express.Router();
+const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
+
 
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
     router.get('/', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
+        res.sendFile(
             path.resolve(__dirname, '../../frontend', 'build', 'index.html')
         );
     });
+
     router.use(express.static(path.resolve("../frontend/build")));
 
     router.get(/^(?!\/?api).*/, (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
+        res.sendFile(
             path.resolve(__dirname, '../../frontend', 'build', 'index.html')
         );
     });
 }
 
+
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
+        res.status(201).json({});
     });
 }
 
