de9ea27ebf0c42e71da9fdf90e9a9e26e8a23742
GaronSmith <wgaronsmith13@gmail.com>
2021-01-25 18:15:27 -0500

    fixed mistake

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -16,6 +16,14 @@ module.exports = (sequelize, DataTypes) => {
         },
       },
     },
+    firstName:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
