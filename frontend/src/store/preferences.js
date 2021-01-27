09e8df8fee96260abbb15213882235e8efbba83f
GaronSmith <wgaronsmith13@gmail.com>
2021-01-27 12:45:14 -0500

    created preferences reducer store

---
+++ frontend/src/store/preferences.js
@@ -0,0 +1,31 @@
+import { fetch } from './csrf'
+
+const SET_PREFERENCES = preferences / setPreferences;
+
+const setPreferences = (preferences) {
+    return {
+        type: SET_PREFERENCES,
+        preferences,
+    }
+}
+
+export const getPreferences = (id) => async (dispatch) => {
+
+    const response = await fetch(`api/preferences/:id`)
+    dispatch(setPreferences(response.data.preferences))
+    return response
+}
+
+const initialState = {};
+
+const preferencesReducer = (state = initialState, action) => {
+    let newState
+    switch (action.type) {
+        case SET_PREFERENCES:
+            newState = action.preferences
+        default:
+            return state;
+    }
+}
+
+export default preferencesReducer
